78b5fca292802c9704c32a11d03f211b4b54b6ec
21stChrom <parkerjmartin2020@gmail.com>
2022-08-25 15:02:02 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-nfw3dh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-nfw3dh)
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import StopWatch from './Components/StopWatch/StopWatch.jsx';
+
+function App() {
+  return (
+    <div className="App">
+     <StopWatch />
+      <iframe id="embed" src="https://react-exdcin.stackblitz.io?embed=1&file=src/style.css?embed=1&file=index.js, src/App.js" className="App"></iframe>
+      
+      </div>
+  );
+  }
+
+export default App;
